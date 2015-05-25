4487e69fc18b5405c73b38fec8d6147884fdbf72
Sayanee <sayanee@gmail.com>
2015-05-25 22:01:16 +0800

    feature: add jshintrc. close #7

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
+'use strict';
+
 var menubar = require('menubar');
 
 var mb = menubar({
   dir: __dirname,
   width: 600,
   height: 480
-})
+});
 
 mb.on('ready', function ready () {
-  console.log('Checkout We Build SG menubar app!')
-})
-
+  console.log('Checkout We Build SG menubar app!');
+});
