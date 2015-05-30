ffd33514ceaed951854f6178161eb8136dbb40bf
Lim Chee Aun <cheeaun@gmail.com>
2015-05-30 15:38:23 +0800

    Remove __dirname and preloadWindow: true

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ var app = require('app');
 var ipc = require('ipc');
 
 var mb = menubar({
-  dir: __dirname,
   width: 600,
   height: 480,
   icon: 'Icon.png',
+  preloadWindow: true
 });
 
 mb.on('ready', function ready () {
